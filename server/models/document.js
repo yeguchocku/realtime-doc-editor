b25fde3f3b73aec2cbcde6603ea076adf8e45f07
yeguchocku <yegammai2006@gmail.com>
2025-12-26 12:53:24 +0530

    attempt 5

--- server/models/document.js
+++ server/models/document.js
@@ -15,4 +15,4 @@ const DocumentSchema = new mongoose.Schema({
   },
 },{timestamps: true});
 
-export default mongoose.model("User", userSchema);
+export default mongoose.model("Document", documentSchema);
